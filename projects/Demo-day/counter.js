c4ca055897fb35b98336d65247014ea550cd7e83
RdanielC <rolandocanales000@gmail.com>
2019-07-15 13:54:29 -0700

    startingcounter

---
+++ projects/Demo-day/counter.js
@@ -0,0 +1,57 @@
+// define vars to hold the values
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+//define var to hold display values
+
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+//create setInterval func
+let interval = null;
+
+
+//stop watch func, logic
+
+function stopWatch() {
+
+    seconds ++;
+
+    //logic to increment values
+
+    if (seconds / 60 === 1){
+        seconds = 0;
+        minutes++;
+
+        if(minutes / 60 === 1){
+            minutes = 0;
+            hours++;
+        }
+    }
+    //adding leading 0
+    if (seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    } else {
+        displaySeconds = seconds;
+    }
+    if (minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    } else {
+        displayMinutes = minutes;
+    }
+    if (hours < 10){
+        displayHours = "0" + hours.toString();
+    } else {
+        displayHours = hours;
+    }
+
+
+
+    //display updated time values
+    document.getElementById('display').innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+
+}
+
+window.setInterval(stopWatch, 1000);
+
