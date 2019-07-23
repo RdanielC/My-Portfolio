b752424bf3b0da2cd969cde25bf582addc64f213
RdanielC <rolandocanales000@gmail.com>
2019-07-23 10:12:26 -0700

    styled instructions, and working leader board

--- projects/Demo-day/leaderBoard.js
+++ projects/Demo-day/leaderBoard.js
@@ -1,3 +1,5 @@
+
+sessionStorage.setItem('namesPrinted', 0); 
 function leader(){
  
     let name = document.querySelector('#table').value;
@@ -10,20 +12,28 @@ function leader(){
     localStorage.setItem(name, total);
     console.log(total);
     for (let i = 0; i < 10; i++) {
+        var totalName = sessionStorage.getItem('namesPrinted')
+        totalName = Number(totalName) + 1
+        sessionStorage.setItem('namesPrinted', totalName)
+        if (totalName > 10) {
+            break;
+        }
         const key = localStorage.key(i)
-        const value = localStorage.getItem(key)
+        if(key == null) {
+            break;
+        }
+        const value = localStorage.getItem(key);
         var item = key + ' ' + value
         var li = document.createElement("li");
         var text = document.createTextNode(item);
         li.appendChild(text);
         document.getElementById('myOL').appendChild(li);
-    // if(i = null) {
-    //     localStorage.setItem(name, "");
-    //     localStorage.setItem(total, "");
-    // }
-    }
-    
         
+
+
+    }
+   
+ 
     
 }
 
